97cc7a93a3c15121b5ef8f15a4996509d05b4dff
Matt <simonmatt1985@gmail.com>
2018-09-12 18:56:28 +0800

    A CRUD example of Angular6 with Nodejs and Mongodb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
+import {
+  Event,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private _loadingBar: SlimLoadingBarService, private _router: Router) {
+    this._router.events.subscribe(event => {
+
+    });
+  }
+
+  private navigationInterceptor(event: Event): void {
+    if (event instanceof NavigationStart) {
+      this._loadingBar.start();
+    }
+    else if (event instanceof NavigationEnd) {
+      this._loadingBar.complete();
+    }
+    else if (event instanceof NavigationError) {
+      this._loadingBar.stop();
+    }
+  }
 }
